a125f10873ba2aff42a801d46e72471e171bd563
Vadim <1strelok3.ru@gmail.com>
2023-06-16 00:45:51 +0300

    testing

--- lab1/constMultiply.h
+++
@@ -1,6 +0,0 @@
-#ifndef LAB1_CONST_MULTIPLY_H
-#define LAB1_CONST_MULTIPLY_H
-
-void constMultiply(void** mas, int* height, int* width, void** scalar, int* type);
-
-#endif //LAB1_CONST_MULTIPLY_H
--- lab1/linearly.h
+++
@@ -1,6 +0,0 @@
-#ifndef LAB1_LINEARLY_H
-#define LAB1_LINEARLY_H
-
-void linearly(void** mas, int* height, int* width, int* type, int* fc, void** sc, int* tc);
-
-#endif //LAB1_LINEARLY_H
