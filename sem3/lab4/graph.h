229b4ceb6e8db1745e92d1890a211c412d1a7516
Vadim <132515284+AveRake@users.noreply.github.com>
2024-01-24 01:34:51 +0300

    Add files via upload

--- sem3/lab4/graph.h
+++ sem3/lab4/graph.h
@@ -1,33 +1,32 @@
 #ifndef LAB4_GRAPH_H
 #define LAB4_GRAPH_H
+
 #include <iostream>
 #include <vector>
 #include <list>
-#include <algorithm>
 #include <set>
 #include <queue>
 
 
+template <typename T>
 class graph {
 private:
     int vertices;
     bool isDirected;
-    std::vector<std::list<int>> adjacencyList;
+    std::vector<std::vector<T>> adjacencyList;
 
 public:
     graph(int vertices, bool isDirected) : vertices(vertices), isDirected(isDirected) {
         adjacencyList.resize(vertices);
     }
 
-
-    void addEdge(int from, int to) {
+    void addEdge(T from, T to) {
         adjacencyList[from].push_back(to);
 
         if (!isDirected) {
             adjacencyList[to].push_back(from);
         }
     }
-
 
     void printGraph() {
         for (int i = 0; i < vertices; ++i) {
@@ -39,12 +38,12 @@ public:
         }
     }
 
-
     void colorGraph() {
         std::vector<int> colors(vertices, -1);
-        std::set<int> availableColors;
 
         for (int vertex = 0; vertex < vertices; ++vertex) {
+            std::set<int> availableColors;
+
             for (const auto& neighbor : adjacencyList[vertex]) {
                 if (colors[neighbor] != -1) {
                     availableColors.insert(colors[neighbor]);
@@ -59,7 +58,6 @@ public:
             }
 
             colors[vertex] = chosenColor;
-            availableColors.clear();
         }
 
         std::cout << "Graph coloring page:" << std::endl;
@@ -67,7 +65,6 @@ public:
             std::cout << "Vertex " << i << " painted in color " << colors[i] << std::endl;
         }
     }
-
 
     std::vector<int> shortestPaths(int startVertex) {
         std::vector<int> distances(vertices, -1);
@@ -89,6 +86,66 @@ public:
         }
 
         return distances;
+    }
+
+    void solveTSP(int startVertex) {
+        std::vector<bool> visited(vertices, false);
+        std::vector<int> path;
+        int totalDistance = 0;
+
+        int currentVertex = startVertex;
+        path.push_back(currentVertex);
+        visited[currentVertex] = true;
+
+        while (path.size() < vertices) {
+            int nearestNeighbor = -1;
+            int minDistance = std::numeric_limits<int>::max();
+
+            for (const auto& neighbor : adjacencyList[currentVertex]) {
+                if (!visited[neighbor] && minDistance > 1) {
+                    nearestNeighbor = neighbor;
+                    minDistance = 1;
+                }
+            }
+
+            if (nearestNeighbor != -1) {
+                path.push_back(nearestNeighbor);
+                visited[nearestNeighbor] = true;
+                totalDistance += 1;
+                currentVertex = nearestNeighbor;
+            } else {
+                bool allVisited = true;
+                for (int i = 0; i < vertices; ++i) {
+                    if (!visited[i]) {
+                        allVisited = false;
+                        break;
+                    }
+                }
+
+                if (allVisited) {
+                    break;
+                }
+
+                for (int i = 0; i < vertices; ++i) {
+                    if (!visited[i]) {
+                        currentVertex = i;
+                        path.push_back(currentVertex);
+                        visited[currentVertex] = true;
+                        break;
+                    }
+                }
+
+                totalDistance += 1;
+            }
+        }
+
+        std::cout << "Approximate solution to the traveling salesman problem (nearest neighbor method): " << std::endl;
+        std::cout << "Path: ";
+        for (const auto& vertex : path) {
+            std::cout << vertex << " ";
+        }
+        std::cout << std::endl;
+        std::cout << "Total distance: " << totalDistance << std::endl;
     }
 };
 
--- sem3/lab4/main.cpp
+++ sem3/lab4/main.cpp
@@ -2,17 +2,19 @@
 
 
 int main() {
-    graph directedGraph(5, true);
-    directedGraph.addEdge(0, 1);
-    directedGraph.addEdge(0, 2);
-    directedGraph.addEdge(1, 3);
-    directedGraph.addEdge(2, 4);
+    graph<double> directedGraph(7, true);
+    directedGraph.addEdge(0.1, 1.1);
+    directedGraph.addEdge(0.1, 2.1);
+    directedGraph.addEdge(1.1, 3.1);
+    directedGraph.addEdge(2.1, 4.1);
+    directedGraph.addEdge(1.1, 5.1);
+    directedGraph.addEdge(2.1, 6.1);
 
     std::cout << "Directed graph:" << std::endl;
     directedGraph.printGraph();
     directedGraph.colorGraph();
+    directedGraph.solveTSP(0);
 
-    std::cout << std::endl;
 
     int startVertex = 0;
     std::vector<int> distances = directedGraph.shortestPaths(startVertex);
@@ -22,22 +24,24 @@ int main() {
         std::cout << "To the top " << i << ": " << distances[i] << std::endl;
     }
 
-    graph undirectedGraph(5, false);
-    undirectedGraph.addEdge(0, 1);
-    undirectedGraph.addEdge(0, 2);
-    undirectedGraph.addEdge(1, 3);
-    undirectedGraph.addEdge(2, 4);
 
-    std::cout << "Undirected graph:" << std::endl;
-    undirectedGraph.printGraph();
-    undirectedGraph.colorGraph();
-
-    distances = undirectedGraph.shortestPaths(startVertex);
-
-    std::cout << "Shortest paths from the top " << startVertex << ":" << std::endl;
-    for (int i = 0; i < distances.size(); ++i) {
-        std::cout << "To the top " << i << ": " << distances[i] << std::endl;
-    }
+//    graph<int> undirectedGraph(5, false);
+//    undirectedGraph.addEdge(0, 1);
+//    undirectedGraph.addEdge(0, 2);
+//    undirectedGraph.addEdge(1, 3);
+//    undirectedGraph.addEdge(2, 4);
+//
+//    std::cout << "Undirected graph:" << std::endl;
+//    undirectedGraph.printGraph();
+//    undirectedGraph.colorGraph();
+//    undirectedGraph.solveTSP(0);
+//
+//    distances = undirectedGraph.shortestPaths(startVertex);
+//
+//    std::cout << "Shortest paths from the top " << startVertex << ":" << std::endl;
+//    for (int i = 0; i < distances.size(); ++i) {
+//        std::cout << "To the top " << i << ": " << distances[i] << std::endl;
+//    }
 
     return 0;
 }
