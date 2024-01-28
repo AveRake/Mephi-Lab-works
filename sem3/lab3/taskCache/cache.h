ebfe6a865eed49aee1e140f409628cb5ec982815
Vadim <132515284+AveRake@users.noreply.github.com>
2024-01-28 22:00:38 +0300

    cache updated

--- sem3/lab3/taskCache/cache.h
+++ sem3/lab3/taskCache/cache.h
@@ -2,7 +2,8 @@
 #define LAB3_CACHE_H
 
 #include "unorderedMap.h"
-
+#include <unordered_map>
+#include <algorithm>
 
 template <typename Key, typename Value>
 class cache {
@@ -13,6 +14,7 @@ public:
         auto it = cacheMap.find(key);
 
         if (cacheMap.size() > 0 && it != cacheMap.end()) {
+            usageCount[key]++;
             cacheList.splice(cacheList.begin(), cacheList, it->second);
             result = it->second->second;
             return true;
@@ -23,14 +25,21 @@ public:
 
     void put(const Key& key, const Value& value) {
         if (cacheMap.size() >= capacity) {
-            auto last = cacheList.end();
-            --last;
-            cacheMap.erase(last->first);
-            cacheList.pop_back();
+            auto minElement = std::min_element(
+                    usageCount.begin(), usageCount.end(),
+                    [](const auto& p1, const auto& p2) {
+                        return p1.second < p2.second;
+                    });
+
+            if (minElement != usageCount.end()) {
+                cacheMap.erase(minElement->first);
+                usageCount.erase(minElement->first);
+            }
         }
 
         cacheList.emplace_front(key, value);
         cacheMap.insert(key, cacheList.begin());
+        usageCount[key] = 1;
     }
 
     void setCapacity(int newCapacity) {
@@ -42,10 +51,16 @@ public:
         capacity = newCapacity;
 
         while (cacheMap.size() > capacity) {
-            auto last = cacheList.end();
-            --last;
-            cacheMap.erase(last->first);
-            cacheList.pop_back();
+            auto minElement = std::min_element(
+                    usageCount.begin(), usageCount.end(),
+                    [](const auto& p1, const auto& p2) {
+                        return p1.second < p2.second;
+                    });
+
+            if (minElement != usageCount.end()) {
+                cacheMap.erase(minElement->first);
+                usageCount.erase(minElement->first);
+            }
         }
     }
 
@@ -53,15 +68,22 @@ public:
         return cacheMap.size();
     }
 
+    [[nodiscard]] int getUsageCount(const Key& key) const {
+        auto it = usageCount.find(key);
+        return (it != usageCount.end()) ? it->second : 0;
+    }
+
     void clear() {
         cacheMap.clear();
         cacheList.clear();
+        usageCount.clear();
     }
 
 private:
     int capacity;
     myUnorderedMap<Key, typename std::list<std::pair<Key, Value>>::iterator> cacheMap;
     std::list<std::pair<Key, Value>> cacheList;
+    std::unordered_map<Key, int> usageCount;
 };
 
 #endif //LAB3_CACHE_H
